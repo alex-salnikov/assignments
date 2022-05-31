abd70c57061d2e5c30d05bc85f12b6e5f05f23f2
alex-salnikov <alexander.salnikov@gmail.com>
2022-05-31 16:34:04 +0200

    week 7

---
+++ w7.py
@@ -0,0 +1,13 @@
+# 1. Implement a function word_list() that reads the 5_letter_words.txt file and returns a list of the words in the file.
+
+# 1. Implement a function word_list() that reads the 5_letter_words.txt file and returns a list of the words in the file.
+# 2. Implement a function random_word() that takes a list of words as a parameter and returns a random word from this list.
+# 3. Implement a function is_real_word() that takes two parameters, a guess and a word list and returns True if the word is in the word list and False otherwise.
+# 4. Implement a function check_guess()that takes two parameters. The first is the guessed word and the second is the word the user has to find. check_guess() returns a string containing the following characters:
+# 5. Implement a function next_guess() that takes a word list as a parameter. The function asks the user for a guess, converts the guess to lower case and checks if the guess is in the word list. If this is the case, the guess is returned. Otherwise, the function asks the user for another guess.
+# 6. Implement a function play() that:
+# - Uses the functions word_list and random_word to select a random 5 letter word.
+# - Asks the user for a guess using the next_guess function.
+# - Checks each guess using the check_guess function and shows the result to the user.
+# - Checks if the users guessed the right word with six guesses or less. If yes, the user wins and the function prints You won!. Otherwise the user loses and the function prints You lost! as well as The word was: followed by word the user had to find.
+
